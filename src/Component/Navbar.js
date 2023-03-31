1114c1b08c209c3dd0c23ecb3fc752b6591f9f07
siamsarker <sarkersiam2@gmail.com>
2023-03-31 12:37:37 +0600

    home updated

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -38,7 +38,7 @@ function Navbar() {
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className='nav-item'>
               <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                HOME
+                Home
               </Link>
             </li>
             <li className='nav-item'>
