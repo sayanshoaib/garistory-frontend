7842065204879484f8556c73b643c1d373a93e7b
siamsarker <sarkersiam2@gmail.com>
2023-03-31 12:34:48 +0600

    Title chaged

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -38,7 +38,7 @@ function Navbar() {
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className='nav-item'>
               <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                Home
+                BOMB
               </Link>
             </li>
             <li className='nav-item'>
