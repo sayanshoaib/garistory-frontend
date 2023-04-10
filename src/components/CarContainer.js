0ff66ef2063d301e8446ed174c0893106eed33d1
siamsarker <sarkersiam2@gmail.com>
2023-04-10 10:41:24 +0600

    format change

--- src/components/CarContainer.js
+++ src/components/CarContainer.js
@@ -1,28 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import CarItem from '../components/CarItem';
-import './CarContainer.css'
+import React, { useEffect, useState } from "react";
+import CarItem from "../components/CarItem";
+import "./CarContainer.css";
 
 const CarContainer = () => {
-    const [cars, setCars] = useState([]);
+  const [cars, setCars] = useState([]);
 
-    useEffect(() => {
-      fetch('car_data.json')
-      .then(res => res.json())
-      .then(data => setCars(data))
-    }, [])
-    
-    return (
-        <div>
-             <div id='carContainer'>
-                {
-                    cars.map(car => <CarItem 
-                        car={car}
-                        key={car.vehicleID}
-                        ></CarItem>)
-                }
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    fetch("car_data.json")
+      .then((res) => res.json())
+      .then((data) => setCars(data));
+  }, []);
+
+  return (
+    <div>
+      <div id="carContainer">
+        {cars.map((car) => (
+          <CarItem car={car} key={car.vehicleID}></CarItem>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CarContainer;
--- src/components/CarItem.js
+++ src/components/CarItem.js
@@ -1,32 +1,30 @@
-import React from 'react';
-import './CarItem.css';
-import { Button } from './../Component/Button';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./CarItem.css";
+import { Button } from "./../Component/Button";
+import { Link } from "react-router-dom";
 
 const CarItem = (props) => {
-    const {make, price, imageURL} = props.car;
-    
-    return (
-        <div className='car'>
-            <img src={imageURL} alt=""/>
-            <div className='car_text'>
-                <h2>{make}</h2>
-                <h3>BDT {price}</h3>
-                
-                <Link to='/carz' className='btn-mobile'>
-                <button
-                    className={`btn btn--test btn--medium`}
-                    // onClick={onClick}
-                    // type={type}
-                >
-                    Details
-                </button>
-                </Link>
-                
-            </div>
-            
-        </div>
-    );
+  const { make, price, imageURL } = props.car;
+
+  return (
+    <div className="car">
+      <img src={imageURL} alt="" />
+      <div className="car_text">
+        <h2>{make}</h2>
+        <h3>BDT {price}</h3>
+
+        <Link to="/carz" className="btn-mobile">
+          <button
+            className={`btn btn--test btn--medium`}
+            // onClick={onClick}
+            // type={type}
+          >
+            Details
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default CarItem;
